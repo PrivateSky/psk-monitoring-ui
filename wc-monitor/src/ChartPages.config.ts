53baf2ceecae97fd8f69050421a58fc03b76fba8
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-10-08 17:19:10 +0300

    Moved chart pages config in separate file

---
+++ wc-monitor/src/ChartPages.config.ts
@@ -0,0 +1,5 @@
+export const chartPages = {
+  Logs: "http://localhost:9999/orgs/02c189808e1a92ab/dashboards/0488702f049cc000?lower=now%28%29%20-%206h&present=true",
+  Resources: "http://localhost:9999/orgs/02c189808e1a92ab/dashboards/0488b075eb5cc000?lower=now%28%29%20-%206h&present=true",
+  Swarms: "http://localhost:9999/orgs/02c189808e1a92ab/dashboards/04886635cb5cc000?lower=now%28%29%20-%206h&present=true"
+};
--- wc-monitor/src/app.config.ts
+++ wc-monitor/src/app.config.ts
@@ -1,8 +1,5 @@
+import {chartPages} from "./ChartPages.config";
 
 export const config = {
-  chartPages: {
-    logs: "http://localhost:9999/orgs/02c189808e1a92ab/dashboards/0488702f049cc000?lower=now%28%29%20-%206h&present=true",
-    resources: "http://localhost:9999/orgs/02c189808e1a92ab/dashboards/0488b075eb5cc000?lower=now%28%29%20-%206h&present=true",
-    swarms: "http://localhost:9999/orgs/02c189808e1a92ab/dashboards/04886635cb5cc000?lower=now%28%29%20-%206h&present=true"
-  }
+  chartPages: chartPages
 };
